2d64ca31bb83ea7123a77266eb9d90a34df7393c
Paisan Aunyaw <paisan@amitysolutions.com>
2024-02-13 13:17:09 +0700

    Split function for run batches

--- src/index.ts
+++ src/index.ts
@@ -10,77 +10,94 @@ import { IData } from './interfaces/file-json.interface';
 
 dotenv.config();
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const batchSize = 100;
+const batchGroupSize = 3;
+const deleyTimeEachBatch = 1000;
+const communityService = new CommunityService();
+
 (async () => {
-  const batchSize = 100;
-  const batchGroupSize = 3;
-  const deleyTimeEachBatch = 1000;
-
-  const communityService = new CommunityService();
   const filePath = path.join(__dirname, `files/rm-user-list-from-community/${process.env.FILE_NAME}.json`)
-  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
   try {
     console.log('Processing file:', `${filePath}`);
     const data = await readJsonFile(filePath);
     const userIds: string[] = [...new Set((data as IData[]).map(d => d.publicId))];
     const batches = splitArrayIntoBatches(userIds, batchSize, batchGroupSize);
-    const batchFailed: any[] = [];
 
-    for (const batch of batches) {
-      await processBatch(batch, communityService, batchFailed, false);
-      await delay(deleyTimeEachBatch);
-
-      if (batchFailed.length) {
-        console.log(`Has error total ${batchFailed.length} batches,  the failed batch will be running again...`);
-        const errorLog: any[] = [];
-        const batches = splitArrayIntoBatches(flatten(batchFailed), 1, batchGroupSize);
-
-        for (const batch of batches) {
-          await processBatch(batch, communityService, errorLog, true);
-          await delay(deleyTimeEachBatch);
-        }
-
-        if(errorLog.length) {
-          const currentDateTimeFormatted = dayjs().format('YYYY-MM-DD_HH-mm-ss');
-          const logFailedBatchPath = path.resolve(__dirname, '../src/logs/failed-batches/');
-          if (!fs.existsSync(logFailedBatchPath)) {
-            fs.mkdirSync(logFailedBatchPath, { recursive: true });
-          }
-
-          const failedBatchFilePath = path.join(logFailedBatchPath, `${currentDateTimeFormatted}-failed.json`);
-          fs.writeFileSync(failedBatchFilePath, JSON.stringify(errorLog, null, 2));
-        }
-      }
-    }
+    await executeBatchesWithRetry(batches);
   } catch (error) {
     console.error(error);
   }
 })();
 
+async function executeBatchesWithRetry(batches: string[][][]) {
+  const failedBatches: any[] = [];
+  for (const batch of batches) {
+    const batchFailed = await processBatch(batch, false);
+    await delay(deleyTimeEachBatch);
+
+    if (batchFailed && batchFailed.length) {
+      failedBatches.push(...batchFailed);
+    }
+  }
+
+  if (failedBatches.length) {
+    console.log(`Has error total ${failedBatches.length} batches,  the failed batch will be running again...`);
+    const batches = splitArrayIntoBatches(flatten(failedBatches), 1, batchGroupSize);
+    await executeBatchesWithLogs(batches);
+  }
+}
+
+async function executeBatchesWithLogs(batches: string[][][]) {
+  const errorLogs: any[] = [];
+  for (const batch of batches) {
+    const batchFailed = await processBatch(batch, true);
+    await delay(deleyTimeEachBatch);
+
+    if (batchFailed && batchFailed.length) {
+      errorLogs.push(...batchFailed);
+    }
+  }
+
+  if (errorLogs.length) {
+    writeLogs(errorLogs);
+  }
+}
+
 async function processBatch(
   batch: string[][],
-  communityService: CommunityService,
-  batchFailed: any[],
   responseErrorLog?: boolean,
-): Promise<void> {
+): Promise<any> {
   const processBatchAsync = async (communityId: string, userIds: string[]) => {
-    try {
-      await communityService.deleteUsersInCommunity(communityId, userIds);
-    } catch (error: any) {
-      if (responseErrorLog) {
-        batchFailed.push({ communityId, userIds, error: error.message });
-      } else {
-        batchFailed.push(userIds);
-      }
-    } finally {
-      console.log(`Processed batch for community ${communityId}, total: ${userIds.length}`);
+  try {
+    await communityService.deleteUsersInCommunity(communityId, userIds);
+  } catch (error: any) {
+    if (responseErrorLog) {
+      return { communityId, userIds, error: error.message }
+    } else {
+      return userIds;
     }
-  };
+  } finally {
+    console.log(`Processed batch for community ${communityId}, total: ${userIds.length}`);
+  }
+};
 
   const batchPromises = batch.map((userIds) => {
     const communityId = process.env.COMMUNITY_ID as string;
     return processBatchAsync(communityId, userIds);
   });
 
-  await Promise.all(batchPromises);
+  const promises = await Promise.all(batchPromises)
+  return promises.filter((p) => p);
+}
+
+function writeLogs(errorLogs: any[]) {
+  const currentDateTimeFormatted = dayjs().format('YYYY-MM-DD_HH-mm-ss');
+  const logFailedBatchPath = path.resolve(__dirname, '../src/logs/failed-batches/');
+  if (!fs.existsSync(logFailedBatchPath)) {
+    fs.mkdirSync(logFailedBatchPath, { recursive: true });
+  }
+
+  const failedBatchFilePath = path.join(logFailedBatchPath, `${currentDateTimeFormatted}-failed.json`);
+  fs.writeFileSync(failedBatchFilePath, JSON.stringify(errorLogs, null, 2));
 }
